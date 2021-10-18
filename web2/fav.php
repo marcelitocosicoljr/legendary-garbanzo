44b584673a7dec15c891ac2a2bbee128496c1fe1
Your Name <mcosicoljr@gmail.com>
2021-10-19 00:35:34 +0800

    done Practice Set Practice 5-01 and Practice 5-02

--- web2/fav.php
+++ web2/fav.php
@@ -2,6 +2,45 @@
 <head><title>Your Favorite</title></head>
 <body>
     <?php
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+              $name= $_POST["username"];
+
+             if (isset($_POST["color"])){
+                $color=$_POST["color"];
+             }else{
+                $color='';
+             }
+
+
+              $dish= $_POST["dish"];
+
+
+              if (empty($name)) {
+                echo "<p>Name is empty</p> <br />";
+              } else {
+                echo "<p>Thanks for your selection ". $name. "</p><br />";
+              }
+
+              if (empty($dish)) {
+                echo "<p>Sharing your favourite dish is happy! :) </p><br />";
+              } else {
+                echo "<p>You really enjoyed ". $dish. "</p><br />";
+              }
+
+              if (empty($color)) {
+                echo "<p>I guess you don't drink alcohol.. :)</p> ";
+              } else {
+                echo "<p>-especially with a nice ". $color. " wine </p><br />";
+              }
+
+
+              
+              
+              
+        }
         
     ?>
 </body>
--- web3/calc.php
+++ web3/calc.php
@@ -2,18 +2,48 @@
 <head><title>Calculation Result</title></head>
 <body>
     <?php
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $val1= $_POST["val1"];
+            $val2= $_POST["val2"];
+
+             if (isset($_POST["calc"])){
+                $calc=$_POST["calc"];
+             }else{
+                $calc='';
+             }
+
+             $result=0;
+              
         # test if input is_numeric
-        if(1){
+        if(is_numeric($val1) && is_numeric($val2)){
             # test if calc is not null
-            if(1){
+            if(!empty($calc)){
                 # switch statement for value of calc
-                switch(1){
-                    
+                switch($calc){
+                    case 'add':
+                        $result= $val1 + $val2; 
+                        break;
+                    case 'sub':
+                        $result= $val1 - $val2; 
+                        break;
+                    case 'mul':
+                        $result= $val1 * $val2; 
+                        break;
+                    case 'div':
+                        $result= $val1 / $val2; 
+                        break;
                 }
-                echo "Calculation result: $result";
+                    echo "Calculation result: ".$result." ";
+            }else{
+                echo "No operation selected. <a href='index.php'>please retry</a>";
             }
         }
-        else{ echo "Invalid entry - please retry"; }
+        else{ echo "Invalid entry - <a href='index.php'>please retry</a>"; }
+
+     }
+
     ?>
 </body>
 </html>
