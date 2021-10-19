c9e28dc14cd4e5c10be9127a0d3fa5545c2bf481
Morpheus <51713799+LittleBolt00816@users.noreply.github.com>
2021-10-19 18:34:07 +0800

    Done Practice 5-05

--- web5/index.php
+++ web5/index.php
@@ -1,7 +1,27 @@
 <?php
+session_start();
+
+    if(!isset($_SESSION['views']))
+    {
+        $_SESSION['views'] = 1;
+    }else {
+        $_SESSION['views']++;
+    }
+
+    if($_SESSION['views'] == 10)
+    {
+        header("Location:congratulations.php");
+    }
+
+    $num_visits = $_SESSION['views'];
+
 
 ?>
 
 <h1>Hello</h1>
 <p>You have visited this page <?= $num_visits ?> times</p>
 <p><a href="?reset=true">Reset</a></p>
+
+<?php if($_SESSION['views'] == 5): ?>
+    <h2>Thank you for visiting the page a lot.</h2>
+<?php endif; ?>
